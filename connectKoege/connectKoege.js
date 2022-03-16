82a98a01fd88fffc12aae7b24d4560438bacc735
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-03-16 18:02:13 +0100

    added popup

---
+++ connectKoege/connectKoege.js
@@ -0,0 +1,9 @@
+function popUp(boxName) {
+    let popupBox = document.getElementById(boxName);
+    let blurBoxes = document.getElementsByClassName('popUpBlur');
+
+    for (let i = 0; i < blurBoxes.length; i++) {
+        blurBoxes[i].classList.toggle('blur');
+    }
+    popupBox.classList.toggle('show');
+  }
